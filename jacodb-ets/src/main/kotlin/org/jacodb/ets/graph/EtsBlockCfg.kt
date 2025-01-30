7b2aad59f16d848fac9d41f1338d34b3a787868e
Konstantin Chukharev <lipen00@gmail.com>
2025-01-30 15:03:45 +0300

    Fix html labels

--- jacodb-ets/src/main/kotlin/org/jacodb/ets/graph/EtsBlockCfg.kt
+++ jacodb-ets/src/main/kotlin/org/jacodb/ets/graph/EtsBlockCfg.kt
@@ -315,10 +315,12 @@ fun EtsBlockCfg.toDot(useHtml: Boolean = true): String {
     blocks.forEach { block ->
         if (useHtml) {
             val s = block.statements.joinToString("") {
-                "<tr><td align=\"left\">" + it.toDotLabel().htmlEncode() + "</td></tr>"
+                it.toDotLabel().htmlEncode() + "<br/>"
             }
-            val h =
-                "<table border=\"0\" cellborder=\"1\" cellspacing=\"0\"><tr><td><b>Block #\\N</b></td></tr>${s}</table>"
+            val h = "<table border=\"0\" cellborder=\"1\" cellspacing=\"0\">" +
+                "<tr><td>" + "<b>Block #\\N</b>" + "</td></tr>" +
+                "<tr><td balign=\"left\">" + s + "</td></tr>" +
+                "</table>"
             lines += "  ${block.id} [label=<${h}>]"
         } else {
             val s = block.statements.joinToString("") { it.toDotLabel() + "\\l" }
