ed1491454c11dba02eaf7c9dd4e6168915f81d07
MForest7 <goalkeeper14631@gmail.com>
2025-02-11 13:19:09 +0300

    remove equals and hashCode

--- jacodb-ets/src/main/kotlin/org/jacodb/ets/model/EtsMethod.kt
+++ jacodb-ets/src/main/kotlin/org/jacodb/ets/model/EtsMethod.kt
@@ -65,17 +65,4 @@ class EtsMethodImpl(
     override fun toString(): String {
         return signature.toString()
     }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as EtsMethodImpl
-
-        return signature == other.signature
-    }
-
-    override fun hashCode(): Int {
-        return signature.hashCode()
-    }
 }
